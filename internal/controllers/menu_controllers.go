3daedc859d23efa718ba0a28d868fcc78e764d13
Zhbert <zhbert@yandex.ru>
2022-12-21 20:48:54 +0300

    Add all necessary data into info block

--- internal/controllers/menu_controllers.go
+++ internal/controllers/menu_controllers.go
@@ -12,6 +12,16 @@ func GitHubPage(context *gin.Context) {
 	viewData := structs.ViewData{}
 
 	viewData.Name = data.Name
+	viewData.FullName = data.FullName
+	viewData.Private = data.Private
+	viewData.Description = data.Description
+	viewData.HomePage = data.Homepage
+	viewData.Stars = data.StargazersCount
+	viewData.Watchers = data.WatchersCount
+	viewData.OpenIssues = data.OpenIssuesCount
+	viewData.Forks = data.ForksCount
+	viewData.License = data.License.Name
+	viewData.GitHubURL = data.HTMLURL
 
 	context.HTML(http.StatusOK, "github.tmpl", gin.H{
 		"title": "GitHub Projects",
--- internal/controllers/structs/view_data.go
+++ internal/controllers/structs/view_data.go
@@ -1,5 +1,15 @@
 package structs
 
 type ViewData struct {
-	Name string
+	Name        string
+	GitHubURL   string
+	FullName    string
+	License     string
+	Private     bool
+	Description string
+	HomePage    string
+	Stars       int
+	Watchers    int
+	OpenIssues  int
+	Forks       int
 }
