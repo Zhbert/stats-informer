4c23179c4d397dad4c11a2394eec84321ad98e64
Zhbert <zhbert@yandex.ru>
2023-01-17 13:48:24 +0300

    Refactor code, add controller for POST form
    Signed-off-by: Zhbert <zhbert@yandex.ru>

---
+++ internal/controllers/github_controllers.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/Zhbert/stats-informer/m/v2/internal/controllers/structs"
+	"github.com/Zhbert/stats-informer/m/v2/internal/services/config"
+	"github.com/Zhbert/stats-informer/m/v2/internal/services/github"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// GetGitHubPage POST Controller of a page with GitHub statistics
+func GetGitHubPage(context *gin.Context) {
+	fmt.Println("TRUE")
+	reposList := config.GetListOfRepos()
+	dataOfRepos := make([]structs.ViewData, 0)
+	for _, url := range reposList {
+		data := github.GetRepoInfo(url)
+		ViewData := structs.ViewData{}
+
+		ViewData.Name = data.Name
+		ViewData.FullName = data.FullName
+		ViewData.Private = data.Private
+		ViewData.Description = data.Description
+		ViewData.HomePage = data.Homepage
+		ViewData.Stars = data.StargazersCount
+		ViewData.Watchers = data.WatchersCount
+		ViewData.OpenIssues = data.OpenIssuesCount
+		ViewData.Forks = data.ForksCount
+		ViewData.License = data.License.Name
+		ViewData.GitHubURL = data.HTMLURL
+
+		dataOfRepos = append(dataOfRepos, ViewData)
+	}
+
+	limit, count := github.GetCountsOfResponses()
+
+	context.HTML(http.StatusOK, "github.tmpl", gin.H{
+		"title":      "GitHub Projects",
+		"data":       dataOfRepos,
+		"respLimit":  limit,
+		"respCount":  count,
+		"totalRepos": len(reposList)})
+}
