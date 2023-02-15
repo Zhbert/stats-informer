712499e1105d30de8d345cb516cd4f926872dbaa
Zhbert <konstantin.nezhbert@flant.com>
2023-02-15 18:37:15 +0300

    Delete unused styles in MainLayout
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -14,32 +14,4 @@ export default {
 };
 </script>
 
-<style lang="css">
-.credits {
-  font-size: 14px;
-  font-weight: normal;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: 1.71;
-  letter-spacing: -0.3px;
-  color: #00122c;
-}
-
-.page__icon {
-  display: inline-block;
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-size: contain;
-}
-
-.page__icon_heart {
-  background-image: url("@/assets/heart.png");
-  width: 12px;
-  height: 12px;
-}
-
-.border_style {
-  border: solid 1px black;
-  border-radius: 5px;
-}
-</style>
+<style lang="css"></style>
