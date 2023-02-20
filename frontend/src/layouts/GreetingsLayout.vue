6dffa7a66a97513b01c7f2803ba96be62fcd1b54
Zhbert <konstantin.nezhbert@flant.com>
2023-02-20 21:40:11 +0300

    Add picture on the greetings page
    Signed-off-by: Zhbert <konstantin.nezhbert@flant.com>

--- frontend/src/layouts/GreetingsLayout.vue
+++ frontend/src/layouts/GreetingsLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="px-4 py-5 my-5 text-center">
+    <img src="@/assets/ant.png" style="width: 35%" class="mb-2" />
     <h1 class="display-5 fw-bold">Welcome to Stats Informer</h1>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">
